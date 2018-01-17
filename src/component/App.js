e35b385c56ed987ba0ac0c73b62e47a6c2cf8f7e
Akash Jain <akash.jain@popxo.com>
2018-01-17 17:27:24 +0530

    Added nested routes

--- src/component/App.js
+++ src/component/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Header from './header/Header';
 import Footer from './footer/Footer';
 import About from './about/About';
-
+import Home from './home/Home';
+import NotFound from './error/NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return(
     	<div className = 'popxo-container'>
-	 		<Header age='5000' />
-	 		<About />
-	 		<Footer />	
+	 			<Header age='5000' />
+	 			<Switch>
+		 			<Route exact path="/" component={Home} />
+					<Route path="/about" component={About} />
+					<Route component={NotFound} />
+					</Switch>
+	 			<Footer />	
  		</div>
  		)
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './component/App'
+import App from './component/App';
+import Home from './component/home/Home';
+import About from './component/about/About';
+import NotFound from './component/error/NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './index.scss'
 
-render(<App/>, document.getElementById('root'))
+const Root = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" component={App} />
+				<Route component={NotFound} />
+			</Switch>
+		</Router>
+	)
+}
+
+render(<Root/>, document.getElementById('root'))
