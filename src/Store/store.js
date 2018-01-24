f59cfa4e814f21268c231b077bc9b39907565f1d
shad-k <shadab.2005@gmail.com>
2018-01-24 16:01:31 +0530

    Add and integrate thunk

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,25 +1,20 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../Reducers/reducer";
 import { syncHistoryWithStore } from 'react-router-redux';
+import thunk from 'redux-thunk';
 
 import createBrowserHistory from 'history/createBrowserHistory'
 
 const customHistory = createBrowserHistory()
 
-const defaultState = {
-	story : [{
-			name: 'story-1',
-			likes: 2
-		},{
-			name: 'story-2',
-			likes: 3
-		},{
-			name: 'story-3',
-			likes: 10
-		}]
-};
-
-const store = createStore(rootReducer, defaultState);
+/* Using thunk let's us return functions as actions
+ * from action creators instead of JSON objects.
+ * This enables us to delay dispatching actions.
+ */
+const store = createStore(
+	rootReducer,
+	applyMiddleware(thunk)
+);
 
 export const history  = syncHistoryWithStore(customHistory, store);
 
